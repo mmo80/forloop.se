4f5652e6dd5705d6d68fd140f62326a4073c3fba
miguel <mmo_80@yahoo.se>
2021-01-01 18:35:30 +0100

    Added Static website

---
+++ Source/StaticWeb/typescript/index.ts
@@ -0,0 +1,25 @@
+﻿import '../sass/app.scss';
+
+// jQuery(function ($) {
+//     $("#contact_form").submit(function () {
+//         var email = $("#email").val();
+//         var msg = $("#msg").val();
+//         $.ajax({
+//             type: "POST",
+//             url: "/api/contact/sendmail",
+//             data: {
+//                 Email: email,
+//                 Message: msg
+//             }
+//         })
+//         .done(function (response) {
+//             alert('Your message has been sent. Thank you!');
+//             $("#email").val('');
+//             $("#msg").val('');
+//         })
+//         .fail(function (response) {
+//             alert('Error sending message.');
+//         });
+//         return false; // prevent page refresh
+//     });
+// });
