171f0565ba547a853ddd2714a099320be986af1f
miguel <mmo_80@yahoo.se>
2021-01-03 23:31:35 +0100

    Added sendgrid integration in function

--- Source/ForloopFunctionApp/SendMail.cs
+++ Source/ForloopFunctionApp/SendMail.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -7,6 +8,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using SendGrid;
+using SendGrid.Helpers.Mail;
 
 namespace ForloopFunctionApp
 {
@@ -17,7 +20,7 @@ namespace ForloopFunctionApp
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("SendMail HTTP trigger function processed a request.");
 
             string email = req.Query["email"];
             string message = req.Query["message"];
@@ -27,11 +30,50 @@ namespace ForloopFunctionApp
             message ??= data?.message;
             email ??= data?.email;
 
-            string responseMessage = string.IsNullOrEmpty(email)
-                ? "This HTTP triggered function executed successfully. Pass a email and message in the query string or in the request body for a personalized response."
-                : $"Hello, {email} and {message}. This HTTP triggered function executed successfully.";
+            if (string.IsNullOrEmpty(email))
+                throw new ArgumentNullException("email empty.");
 
+            if (string.IsNullOrEmpty(message))
+                throw new ArgumentNullException("message empty.");
+
+            await SendEmail(message, email, GetToEmail());
+
+            var responseMessage = "Message sent successfully.";
             return new OkObjectResult(new SendMailResponse { Message = responseMessage });
+        }
+
+        private static string GetToEmail()
+        {
+            var value = Environment.GetEnvironmentVariable("SendToMail");
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException($"No ToEmail-Key found in settings!");
+
+            return value;
+        }
+
+        private static string GetApiKey()
+        {
+            var value = Environment.GetEnvironmentVariable("SendGridApiKey");
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentNullException($"No Api-Key found in settings!");
+
+            return value;
+        }
+
+        public static async Task SendEmail(string plainTextContent, string fromEmail, string toEmail)
+        {
+            var apiKey = GetApiKey();
+            var client = new SendGridClient(apiKey);
+            var from = new EmailAddress(fromEmail);
+            var subject = "Forloop Contact Form";
+            var to = new EmailAddress(toEmail);
+            var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, "");
+            var response = await client.SendEmailAsync(msg);
+
+            if (response.StatusCode != HttpStatusCode.Accepted && response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception($"Failed to send email. StatusCode:{response.StatusCode}, Message:{response.Body}");
+            }
         }
     }
 
